66ed1136b7f88332f335eb868f4fa29e955e403d
Daniel Boadzie <boadziedaniel43@gmail.com>
2020-10-17 16:46:11 +0000

    added link to my portfolio

--- app.py
+++ app.py
@@ -40,10 +40,11 @@ def cloud(image, text, max_word, max_font, random):
 
 def main():
     st.write("# Text Summarization with a WordCloud")
+    st.write("[By Boadzie Daniel](https://boadzie.surge.sh)")
     max_word = st.sidebar.slider("Max words", 200, 3000, 200)
     max_font = st.sidebar.slider("Max Font Size", 50, 350, 60)
     random = st.sidebar.slider("Random State", 30, 100, 42 )
-    image = st.file_uploader("Choose a file(preferably a silhouette")
+    image = st.file_uploader("Choose a file(preferably a silhouette)")
     text = st.text_area("Add text ..")
     if image and text is not None:
         if st.button("Plot"):
